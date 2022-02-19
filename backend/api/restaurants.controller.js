52a85dba9c98c10ef0e88d364fa8420204d4a8c3
viktor-bobryk <victor.bobryk@sigma.software>
2022-02-19 19:06:05 +0200

    finished backend part

--- backend/api/restaurants.controller.js
+++ backend/api/restaurants.controller.js
@@ -1,7 +1,7 @@
 import RestaurantsDAO from "../dao/restaurantsDAO.js"
 
 export default class RestaurantsController {
-    static async apiGetRestaurants(req, res, next) {
+    static async apiGetRestaurants(req, res) {
         const restaurantsPerPage = req.query.restaurantsPerPage ? parseInt(req.query.restaurantsPerPage, 10) : 20
         const page = req.query.page ? parseInt(req.query.page, 10) : 0
 
@@ -28,5 +28,30 @@ export default class RestaurantsController {
             total_results: totalNumRestaurants,
         }
         res.json(response)
+    }
+
+    static async apiGetRestaurantById(req, res) {
+        try {
+            let id = req.params.id || {}
+            let restaurant = await RestaurantsDAO.getRestaurantByID(id)
+            if (!restaurant) {
+                res.status(404).json({ error: "Not found" })
+                return
+            }
+            res.json(restaurant)
+        } catch (e) {
+            console.log(`api, ${e}`)
+            res.status(500).json({ error: e })
+        }
+    }
+
+    static async apiGetRestaurantCuisines(req, res) {
+        try {
+            let cuisines = await RestaurantsDAO.getCuisines()
+            res.json(cuisines)
+        } catch (e) {
+            console.log(`api, ${e}`)
+            res.status(500).json({ error: e })
+        }
     }
 }
